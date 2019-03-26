496618fa103c169babd229fcb0e01ab7ed345cb5
Stanislav Kovalevsky <kov.stas99@gmail.com>
2019-03-26 21:02:21 +0300

    1 - Top-level instance (Adding macro and updating run_test function)

--- tb_top.sv
+++ tb_top.sv
@@ -1,5 +1,6 @@
 module tb_top;
    import uvm_pkg::*;
+   `define CLK_HALF_PERIOD 10ns
  
 	bit i_cs_n;
 	bit i_clk;
@@ -8,13 +9,13 @@ module tb_top;
 	bit i_wp_n;
 	logic i_di;
 	logic o_do;
-   always #10 i_clk <= ~i_clk; 
+   always `CLK_HALF_PERIOD i_clk <= ~i_clk;
  
-   spi_flash_if         spi_f_if  (i_cs_n, i_clk, i_hold_n, i_rst_n, i_wp_n, i_di, o_do);
-   spi_flash_wrapper    spi_f_wrapper  (._if (spi_f_if));
+   spi_flash_if      spi_f_if(i_cs_n, i_clk, i_hold_n, i_rst_n, i_wp_n, i_di, o_do);
+   spi_flash_wrapper spi_f_wrapper(._if (spi_f_if));
 
    initial begin
       uvm_config_db #(virtual spi_flash_if)::set (null, "uvm_test_top", "spi_flash_if", spi_f_if);
-      run_test ("base_test");
+      run_test();
    end
 endmodule
