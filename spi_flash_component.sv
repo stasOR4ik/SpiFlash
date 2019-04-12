d4a02a361e1db5b1b2693c82a15acc7aebfedaf8
Stanislav Kovalevsky <kov.stas99@gmail.com>
2019-04-13 00:50:40 +0300

    7 - Add analysis imp instance into model component

--- spi_flash_component.sv
+++ spi_flash_component.sv
@@ -1,11 +1,14 @@
 `ifndef SPI_FLASH_COMPONENT
 `define SPI_FLASH_COMPONENT
 
-class spi_flash_component extends uvm_component ;
+class spi_flash_component #(type T = uvm_sequence_item) extends uvm_component;
 	`uvm_component_utils(spi_flash_component)
 	//`uvm_component_utils_begin(spi_flash_component)
 	 /**** `uvm_field_* macro invocations here ****/
 	//`uvm_component_utils_end
+
+	typedef spi_flash_component #(T) this_type;
+	uvm_analysis_imp#(T, this_type) ap;
 
 	function new(string name = "spi_flash_component", uvm_component parent=null);
 		super.new(name, parent);
@@ -25,7 +28,10 @@ class spi_flash_component extends uvm_component ;
 
     	phase.drop_objection(this);
 	endtask
+
+	function void write(T t);
+
+	endfunction
 endclass
 
 `endif
-
