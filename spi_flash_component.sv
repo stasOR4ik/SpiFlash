d81b9b18e273106ceb3be96c3818f0f6aeaea6ae
Stanislav Kovalevsky <kov.stas99@gmail.com>
2019-04-13 14:57:41 +0300

    7 - Add analysis imp instance into model component (Deleting parameters)

--- spi_flash_component.sv
+++ spi_flash_component.sv
@@ -1,14 +1,12 @@
 `ifndef SPI_FLASH_COMPONENT
 `define SPI_FLASH_COMPONENT
 
-class spi_flash_component #(type T = uvm_sequence_item) extends uvm_component;
+class spi_flash_component extends uvm_component;
 	`uvm_component_utils(spi_flash_component)
-	//`uvm_component_utils_begin(spi_flash_component)
-	 /**** `uvm_field_* macro invocations here ****/
-	//`uvm_component_utils_end
 
-	typedef spi_flash_component #(T) this_type;
-	uvm_analysis_imp#(T, this_type) ap;
+	typedef uvm_sequence_item transaction_type;
+
+	uvm_analysis_imp#(transaction_type, spi_flash_component) ap;
 
 	function new(string name = "spi_flash_component", uvm_component parent=null);
 		super.new(name, parent);
@@ -16,6 +14,7 @@ class spi_flash_component #(type T = uvm_sequence_item) extends uvm_component;
 
 	virtual function void build_phase(uvm_phase phase);
 		super.build_phase(phase);
+		ap = new("ap", this);
 	endfunction
 
 	virtual function void connect_phase(uvm_phase phase);
@@ -29,7 +28,7 @@ class spi_flash_component #(type T = uvm_sequence_item) extends uvm_component;
     	phase.drop_objection(this);
 	endtask
 
-	function void write(T t);
+	function void write(transaction_type transaction);
 
 	endfunction
 endclass
