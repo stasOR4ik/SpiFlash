e19ad9fb9f6a26ed7ef8e4597112f38d4a520d25
ArsenyZhivitsky <36786913+ArsenyZhivitsky@users.noreply.github.com>
2019-04-22 16:15:27 +0300

    2 - Sequence_item class (Adding uvm_sequence_item extends)

--- spi_flash_transaction.sv
+++ spi_flash_transaction.sv
@@ -1,9 +1,20 @@
 `ifndef SPI_TRANSACTION
 `define SPI_TRANSACTION
 
-class sequence_item;
+class spi_transaction extends uvm_sequence_item;
+
   byte data;
   time tr_beginning;
-endclass;
+
+  `uvm_object_utils_begin(spi_transaction)
+    `uvm_field_int(data, UVM_ALL_ON)
+    `uvm_field_int(tr_beginning, UVM_ALL_ON)
+  `uvm_object_utils_end
+
+  function new(string name="spi_transaction");
+    super.new(name);
+  endfunction
+
+endclass
 
 `endif
